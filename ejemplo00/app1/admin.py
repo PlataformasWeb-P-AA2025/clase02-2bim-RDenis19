57cc42ee1a91d084dbf50a42ad94f7d9b470df8a
Denis Ruiz <dlruiz7@utpl.edu.ec>
2025-06-11 08:51:24 -0500

    clase02

--- ejemplo00/app1/admin.py
+++ ejemplo00/app1/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Importar las clases del modelo
 from app1.models import Estudiante
+from app1.models import Ciudad
 
 admin.site.register(Estudiante)
+admin.site.register(Ciudad)
